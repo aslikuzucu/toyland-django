c87957791b3f9e32362c03f2ec792b52b6d42afa
Asli <110307191+aslikuzucu@users.noreply.github.com>
2024-05-03 17:14:50 +0300

    django authentication

--- toyland/settings.py
+++ toyland/settings.py
@@ -136,3 +136,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT=BASE_DIR/'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'toyland.mail.asli@gmail.com'
+EMAIL_HOST_PASSWORD = 'uzxz yzdm joku pdtj'
+EMAIL_USE_TLS = True
